2847124cce4a8964e1a1878a2976f74a4aec3c04
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-24 16:11:26 +0530

    Create CryptoMarketWidget.js

---
+++ components/CryptoMarketWidget.js
@@ -0,0 +1,28 @@
+import Script from "next/script";
+import { useState } from "react";
+
+export default function CryptoMarketWidget({ height }) {
+
+    return <>
+
+        <div id="crypto-market-widget" style={{ width: "100%", height: "100%" }}>
+            <Script
+                id="crypto-market-script"
+                src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js"
+                onLoad={() => {
+                    document.getElementById('crypto-market-widget').appendChild(document.getElementById('crypto-market-script'));
+                }}>
+                {JSON.stringify({
+                    "width": '100%',
+                    "height": height,
+                    "defaultColumn": "overview",
+                    "screener_type": "crypto_mkt",
+                    "displayCurrency": "USD",
+                    "colorTheme": "light",
+                    "locale": "en",
+                })}
+            </Script>
+        </div>
+
+    </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import CryptoMarketWidget from "../components/CryptoMarketWidget";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [height,setHeight] = useState(null)
+  
+  useEffect(()=>{
+
+    if (window.innerWidth < 500) setHeight(300)
+    else setHeight(500)
+
+  },[])
+
+
 
   return (
     <>
@@ -9,7 +22,7 @@ export default function Home() {
 
       <Header />
 
-      <div>Hello</div>
+      {height && <CryptoMarketWidget height={height} />}
     </>
   )
 }
