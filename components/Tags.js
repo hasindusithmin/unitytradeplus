0aeb5800b8c5b8191d35e313f5ecf0519b4fa7b3
hasindusithmin <hasindusithmin64@gmail.com>
2022-10-29 00:25:55 +0530

    Update index.js

--- components/Tags.js
+++ components/Tags.js
@@ -3,7 +3,7 @@ import icons from "../public/icons.json";
 export default function Tags() {
 
     return (
-        <p>
+        <p className="w3-center">
             {
                 Object.values(icons).map(icon=><a  key={icon} href={`/crypto/${icon}`} ><span className="w3-tag w3-margin-bottom w3-white"><img src={`/icons/${icon}.svg`} alt={icon} width={32} height={32} title={icon.toUpperCase()} />&nbsp;&nbsp;&nbsp;</span></a>)
             }
