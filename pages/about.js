045f0c5e46dc7c844ce15651d927751a51a982a6
1Aaqil <ammsaaqil1998@gmail.com>
2022-11-12 17:37:36 +0530

    updat about.js(I have included our mission ,vision,who we are information in about us page)

--- pages/about.js
+++ pages/about.js
@@ -7,6 +7,25 @@ export default function About(){
                 <title>unitytrade+ | about</title>
             </Head>
             <Header></Header>
+            <div className="w3-row w3-padding w3-border"> 
+            <div className="w3-white w3-margin">             
+              <div className="w3-container w3-black">
+              <h4>WHO ARE WE</h4>
+              </div>
+            </div>
+            <div className="w3-white w3-margin">             
+              <div className="w3-container w3-black">
+              <h4>OUR MISSION</h4>
+              </div>
+            </div>
+            
+            <div className="w3-white w3-margin">             
+              <div className="w3-container w3-black">
+              <h4>OUR VISION</h4>
+              </div>
+            </div>
+
+            </div>
         </>
     )
 
