09639f47de496e1c999b3e18f58b3ed632850fb5
1Aaqil <ammsaaqil1998@gmail.com>
2022-11-12 17:10:33 +0530

    create about.js

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Head from "next/head";
+export default function About(){
+    return(
+        <>  
+            <Head>
+                <title>unitytrade+ | about</title>
+            </Head>
+            <Header></Header>
+        </>
+    )
+
+}
